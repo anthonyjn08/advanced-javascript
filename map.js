f2ef77afdf364c72f7ca0c40daa935815abcc044
anthonyjn08 <anthonyjnelson@hotmail.com>
2022-04-08 21:33:20 +0000

    Advanced js added

---
+++ map.js
@@ -0,0 +1,53 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node map.js in the terminal
+ */
+
+// Using a for loop
+let nums = [1, 2, 3, 4, 5,];
+let results = [];
+for (let num of nums) {
+    results.push(num * 2);
+}
+console.log(results)
+
+// Using map()
+const multbyTwo = function (num) {
+    return num * 2;
+}
+
+const mapResults = nums.map(multbyTwo)
+console.log(mapResults)
+
+// Simplified w/ map()
+const simplified = nums.map(function (num) { return num * 2});
+console.log(simplified)
+
+// Simplfied w/ map() + arrow function
+const results2 = nums.map(num => num * 2)
+console.log(results2)
+
+// With objects:
+const students = [
+    {
+        id: 1,
+        name: 'Mark',
+        profession: 'Developer',
+        skill: 'JavaScript'
+    },
+    {
+        id: 2,
+        name: 'Ariel',
+        profession: 'Developer',
+        skill: 'HTML'
+    },
+    {
+        id: 3,
+        name: 'Jason',
+        profession: 'Designer',
+        skill: 'CSS'
+    },
+];
+
+const studentsWithIds = students.map(people => [people.name, people.id]);
+console.log(studentsWithIds)
